e4dfe8dd7816e932158c50c2d988ac0d67913c8f
Student <bwilliams1@my.yearupunited.org>
2025-12-17 09:16:02 -0500

    Day 3 of Capstone 3 Commit

--- CapstoneStarterAPI/src/main/java/org/yearup/controllers/ProductsController.java
+++ CapstoneStarterAPI/src/main/java/org/yearup/controllers/ProductsController.java
@@ -72,7 +72,7 @@ public class ProductsController
     {
         try
         {
-            productDao.create(product);
+            productDao.update(id, product);
         }
         catch(Exception ex)
         {
--- CapstoneStarterAPI/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ CapstoneStarterAPI/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -26,12 +26,12 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
         String sql = "SELECT * FROM products " +
                 "WHERE (category_id = ? OR ? = -1) " +
                 "   AND (price >= ? OR ? = -1) " +
-                "   AND (subcategory = ? OR ? = '') " +
-                "   ABD (price <= ? or ? = -1";
+                "   AND (price <= ? or ? = -1) " +
+                "   AND (subcategory = ? OR ? = '') ";
 
         categoryId = categoryId == null ? -1 : categoryId;
         minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
-        maxPrice = maxPrice == null ? new BigDecimal("1") : maxPrice;
+        maxPrice = maxPrice == null ? new BigDecimal("-1") : maxPrice;
         subCategory = subCategory == null ? "" : subCategory;
 
         try (Connection connection = getConnection())
@@ -41,10 +41,10 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
             statement.setInt(2, categoryId);
             statement.setBigDecimal(3, minPrice);
             statement.setBigDecimal(4, minPrice);
-            statement.setString(5, subCategory);
-            statement.setString(6, subCategory);
-            statement.setBigDecimal(7, maxPrice);
-            statement.setBigDecimal(8, maxPrice);
+            statement.setBigDecimal(5, maxPrice);
+            statement.setBigDecimal(6, maxPrice);
+            statement.setString(7, subCategory);
+            statement.setString(8, subCategory);
 
             ResultSet row = statement.executeQuery();
 
@@ -210,8 +210,7 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
         }
     }
 
-    protected static Product mapRow(ResultSet row) throws SQLException
-    {
+    protected static Product mapRow(ResultSet row) throws SQLException {
         int productId = row.getInt("product_id");
         String name = row.getString("name");
         BigDecimal price = row.getBigDecimal("price");
